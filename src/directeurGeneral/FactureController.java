e9922cd067e7a80186d5f29a44722684f9e880b4
gaye <etservi@yahoo.com>
2018-08-21 15:10:28 +0000

    Add Facture

--- src/directeurGeneral/FactureController.java
+++ src/directeurGeneral/FactureController.java
@@ -33,7 +33,7 @@ public class FactureController implements Initializable{
 	
 	@FXML private AnchorPane rootPane;
 	
-	@FXML private TableView<Article> tbViewFacture;
+	@FXML private static TableView<Article> tbViewFacture;
 	
 	@FXML private TableColumn<Article, Integer> tcIdArticle;
 	@FXML private TableColumn<Article, String> tcNom;
@@ -94,7 +94,7 @@ public class FactureController implements Initializable{
 	final ObservableList<String> listPurchase = FXCollections.observableArrayList();
 	final ObservableList<Object> listOfPrice = FXCollections.observableArrayList();
 	
-	Double total = 0.0;
+	static Double total = 0.0;
 	
 	public void ajouterArticle() {
 		   String articleName;
@@ -127,42 +127,41 @@ public class FactureController implements Initializable{
 	        Double prixUnit = Double.valueOf( prixUnitairee.getText() );
 	        
 	        Article model = new Article(idA, momArtcle, qtte, prixUnit);
-	        tbViewFacture.getItems().addAll(model);
-	        
-/*				
-	tbViewFacture.setItems( listPurchase );
-	tbViewFacture.setItems(String.valueOf(listOfPrice));
-
-	numberOfArticlee = (listPurchase.get(listPurchase.indexOf(numberOfArticlee))).length();		   
-		   
-	numberOfArticlee = listOfPrice.getItemCount();
-	numberOfArticlee = listPurchase.get(String.valueOf(nombreProduit));
-	qtite.setText(String.valueOf(numberOfArticlee));
-*/    		   
+	        tbViewFacture.getItems().addAll(model);	   
 	  
-		   total += amount;
-		   refPrixTotal.setText( String.valueOf(total) + "  F CFA " ); // DISPALY TOTAL AMOUNT IN TEXTTFIELD
-		   
-			
-		  
-	   }
+	total += amount;
+	refPrixTotal.setText( String.valueOf(total) + "  F CFA " ); // DISPALY TOTAL AMOUNT IN TEXTTFIELD
+		     
+	}
 	   
-	   public void annulerArticle() {
+	   @SuppressWarnings("unlikely-arg-type")
+	public void annulerArticle() {
 		   int index, nombre;
 		   Double amount, price;
-		  /* 
+		 /*  
 		   nombre = Integer.parseInt(qtite.getText());
 		   index = listPurchase.getSelectedInde();
 		   amount = Double.parseDouble(listOfPrice.getItem(index));
-		   
-		   nombre = nombre - 1;
-		   total = total - amount;
-		   
-		   listPurchase.remove(index);
-		   listOfPrice.remove(index);
-		   qtite.setText(String.valueOf(nombre));
-		   refPrixTotal.setText(String.valueOf(total));   
 		   */
+		   //
+		   String quantite = comboBoxQuatite.getSelectionModel().getSelectedItem().toString(); // RECUPERATION VALEUR COMBOBOX
+		   Article indexSelctedTab = tbViewFacture.getSelectionModel().getSelectedItem();
+//		   amount = price * Integer.parseInt(quantite);  // CALCUL MONTANT TOTAL
+		   //
+//		  int quantitee = Integer.parseInt(quantite) - 1;
+//		   nombre = nombre - 1;
+//		   total = total - amount;
+		   
+		   listPurchase.remove(indexSelctedTab);
+		   listOfPrice.remove(quantite);
+		//--------------------------------------   
+		   System.out.println("Article seletionner " + indexSelctedTab);  
+//		   System.out.println("Total"+amount); System.out.println("Montant Ttal restant"+ total);
+		   
+//		   qtite.setText(String.valueOf(nombre));
+		   refPrixTotal.setText(String.valueOf(total));   
+		//----------------------------------------------------------------------------   
+		   
 	   }
 
 	//------------------------------------------
